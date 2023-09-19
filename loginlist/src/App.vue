42fef2516d0a0a617b7a0c2b2422006ffb74f0a4
EduardoMurillo3626 <luis_murillo3626@hotmail.com>
2023-09-19 15:48:19 -0700

    se agrego

---
+++ loginlist/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>Login</h1>
+  <LoginLista></LoginLista>
+  
+</template>
+
+<script lang="ts" setup>
+import LoginLista from "./components/LoginLista.vue";
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
